b841fe68aab62719a2180303dad2f1effb1d74a0
Ted Halmrast <ted@dimensiology.com>
2015-01-02 16:37:59 -0600

    Added build files for internal testing & use via bower.

--- build/three.js
+++ build/three.js
@@ -21712,6 +21712,10 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 	}
 
+  THREE.clearTextureUnits = function() {
+		_usedTextureUnits = 0;
+  };
+
 	function setProgram( camera, lights, fog, material, object ) {
 
 		_usedTextureUnits = 0;
@@ -22589,6 +22593,7 @@ THREE.WebGLRenderer = function ( parameters ) {
 		}
 
 	}
+  THREE.loadUniformsGeneric = loadUniformsGeneric;
 
 	function setupMatrices ( object, camera ) {
 
@@ -25458,17 +25463,17 @@ THREE.ShadowMapPlugin = function ( _renderer, _lights, _webglObjects, _webglObje
  * @author mikael emtinger / http://gomo.se/
  * @author alteredq / http://alteredqualia.com/
  */
-
 THREE.SpritePlugin = function ( renderer, sprites ) {
 
 	var gl = renderer.context;
+ 
+  var vertexBuffer, elementBuffer;
+	var defaultProgram;
+  var programs = [];
+  
+  var texture;
 
-	var vertexBuffer, elementBuffer;
-	var program, attributes, uniforms;
-
-	var texture;
-
-	// decompose matrixWorld
+  // decompose matrixWorld
 
 	var spritePosition = new THREE.Vector3();
 	var spriteRotation = new THREE.Quaternion();
@@ -25477,10 +25482,10 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 	var init = function () {
 
 		var vertices = new Float32Array( [
-			- 0.5, - 0.5,  0, 0,
-			  0.5, - 0.5,  1, 0,
-			  0.5,   0.5,  1, 1,
-			- 0.5,   0.5,  0, 1
+			- 0.5, - 0.5, 0, 0, 
+			  0.5, - 0.5, 1, 0,
+			  0.5,   0.5, 1, 1,
+			- 0.5,   0.5, 0, 1
 		] );
 
 		var faces = new Uint16Array( [
@@ -25497,40 +25502,13 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 		gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, elementBuffer );
 		gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, faces, gl.STATIC_DRAW );
 
-		program = createProgram();
-
-		attributes = {
-			position:			gl.getAttribLocation ( program, 'position' ),
-			uv:					gl.getAttribLocation ( program, 'uv' )
-		};
-
-		uniforms = {
-			uvOffset:			gl.getUniformLocation( program, 'uvOffset' ),
-			uvScale:			gl.getUniformLocation( program, 'uvScale' ),
-
-			rotation:			gl.getUniformLocation( program, 'rotation' ),
-			scale:				gl.getUniformLocation( program, 'scale' ),
-
-			color:				gl.getUniformLocation( program, 'color' ),
-			map:				gl.getUniformLocation( program, 'map' ),
-			opacity:			gl.getUniformLocation( program, 'opacity' ),
-
-			modelViewMatrix: 	gl.getUniformLocation( program, 'modelViewMatrix' ),
-			projectionMatrix:	gl.getUniformLocation( program, 'projectionMatrix' ),
-
-			fogType:			gl.getUniformLocation( program, 'fogType' ),
-			fogDensity:			gl.getUniformLocation( program, 'fogDensity' ),
-			fogNear:			gl.getUniformLocation( program, 'fogNear' ),
-			fogFar:				gl.getUniformLocation( program, 'fogFar' ),
-			fogColor:			gl.getUniformLocation( program, 'fogColor' ),
-
-			alphaTest:			gl.getUniformLocation( program, 'alphaTest' )
-		};
-
+    defaultProgram = new WebGLSpriteProgram();
+    programs.push( defaultProgram );
+		
 		var canvas = document.createElement( 'canvas' );
 		canvas.width = 8;
 		canvas.height = 8;
-
+		
 		var context = canvas.getContext( '2d' );
 		context.fillStyle = 'white';
 		context.fillRect( 0, 0, 8, 8 );
@@ -25542,95 +25520,127 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 	this.render = function ( scene, camera ) {
 
-		if ( sprites.length === 0 ) return;
+    if ( sprites.length === 0 ) return;
 
-		// setup gl
+    if ( defaultProgram === undefined ) {
 
-		if ( program === undefined ) {
+      init();
 
-			init();
+    }
+
+
+		// update positions and sort
+
+    var i, numSprites = sprites.length, sprite;
+
+		for ( i = 0; i < numSprites; i ++ ) {
+
+			sprite = sprites[ i ];
+
+			sprite._modelViewMatrix.multiplyMatrices( camera.matrixWorldInverse, sprite.matrixWorld );
+			sprite.z = - sprite._modelViewMatrix.elements[ 14 ];
+
+      var fragmentShader = sprite.material.fragmentShader;
+      if ( fragmentShader && !sprite.program ) {
+
+        sprite.program = resolveShader( fragmentShader, sprite.material.fragmentUniforms );
+
+      }
 
 		}
 
-		gl.useProgram( program );
 
-		gl.enableVertexAttribArray( attributes.position );
-		gl.enableVertexAttribArray( attributes.uv );
+		sprites.sort( painterSortStable );
+
+
+		// setup gl
 
 		gl.disable( gl.CULL_FACE );
 		gl.enable( gl.BLEND );
 
-		gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
-		gl.vertexAttribPointer( attributes.position, 2, gl.FLOAT, false, 2 * 8, 0 );
-		gl.vertexAttribPointer( attributes.uv, 2, gl.FLOAT, false, 2 * 8, 8 );
-
+	  gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
 		gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, elementBuffer );
-
-		gl.uniformMatrix4fv( uniforms.projectionMatrix, false, camera.projectionMatrix.elements );
-
 		gl.activeTexture( gl.TEXTURE0 );
-		gl.uniform1i( uniforms.map, 0 );
+
 
 		var oldFogType = 0;
 		var sceneFogType = 0;
 		var fog = scene.fog;
 
-		if ( fog ) {
+    programs.forEach( function ( program ) {
+		  gl.useProgram( program.program );
 
-			gl.uniform3f( uniforms.fogColor, fog.color.r, fog.color.g, fog.color.b );
+		  gl.enableVertexAttribArray( program.attributes.position );
+		  gl.enableVertexAttribArray( program.attributes.uv );
 
-			if ( fog instanceof THREE.Fog ) {
-
-				gl.uniform1f( uniforms.fogNear, fog.near );
-				gl.uniform1f( uniforms.fogFar, fog.far );
-
-				gl.uniform1i( uniforms.fogType, 1 );
-				oldFogType = 1;
-				sceneFogType = 1;
-
-			} else if ( fog instanceof THREE.FogExp2 ) {
-
-				gl.uniform1f( uniforms.fogDensity, fog.density );
-
-				gl.uniform1i( uniforms.fogType, 2 );
-				oldFogType = 2;
-				sceneFogType = 2;
-
-			}
-
-		} else {
-
-			gl.uniform1i( uniforms.fogType, 0 );
-			oldFogType = 0;
-			sceneFogType = 0;
-
-		}
+		  gl.vertexAttribPointer( program.attributes.position, 2, gl.FLOAT, false, 2 * 8, 0 );
+		  gl.vertexAttribPointer( program.attributes.uv, 2, gl.FLOAT, false, 2 * 8, 8 );
 
 
-		// update positions and sort
+		  gl.uniformMatrix4fv( program.uniforms.projectionMatrix, false, camera.projectionMatrix.elements );
 
-		for ( var i = 0, l = sprites.length; i < l; i ++ ) {
+		  gl.uniform1i( program.uniforms.map, 0 );
 
-			var sprite = sprites[ i ];
+		  if ( fog ) {
 
-			sprite._modelViewMatrix.multiplyMatrices( camera.matrixWorldInverse, sprite.matrixWorld );
-			sprite.z = - sprite._modelViewMatrix.elements[ 14 ];
+			  gl.uniform3f( program.uniforms.fogColor, fog.color.r, fog.color.g, fog.color.b );
 
-		}
+			  if ( fog instanceof THREE.Fog ) {
 
-		sprites.sort( painterSortStable );
+				  gl.uniform1f( program.uniforms.fogNear, fog.near );
+				  gl.uniform1f( program.uniforms.fogFar, fog.far );
+
+				  gl.uniform1i( program.uniforms.fogType, 1 );
+				  oldFogType = 1;
+				  sceneFogType = 1;
+
+			  } else if ( fog instanceof THREE.FogExp2 ) {
+
+				  gl.uniform1f( program.uniforms.fogDensity, fog.density );
+
+				  gl.uniform1i( program.uniforms.fogType, 2 );
+				  oldFogType = 2;
+				  sceneFogType = 2;
+
+			  }
+
+		  } else {
+
+			  gl.uniform1i( program.uniforms.fogType, 0 );
+			  oldFogType = 0;
+			  sceneFogType = 0;
+
+		  }
+    });
+
 
 		// render all sprites
 
 		var scale = [];
+    var program;
 
-		for ( var i = 0, l = sprites.length; i < l; i ++ ) {
+		for ( i = 0; i < numSprites; i ++ ) {
 
-			var sprite = sprites[ i ];
+			sprite = sprites[ i ];
 			var material = sprite.material;
 
-			gl.uniform1f( uniforms.alphaTest, material.alphaTest );
-			gl.uniformMatrix4fv( uniforms.modelViewMatrix, false, sprite._modelViewMatrix.elements );
+      var spriteProgram = sprite.program || defaultProgram;
+      if ( sprite.program || program !== spriteProgram ) {
+
+        program = spriteProgram;
+        gl.useProgram( program.program );
+        THREE.clearTextureUnits();
+      }
+
+      var fragmentUniforms = material.fragmentUniforms;
+      if ( fragmentUniforms ) {
+
+        program.loadUniformsGeneric( fragmentUniforms );
+
+      }
+
+			gl.uniform1f( program.uniforms.alphaTest, material.alphaTest );
+			gl.uniformMatrix4fv( program.uniforms.modelViewMatrix, false, sprite._modelViewMatrix.elements );
 
 			sprite.matrixWorld.decompose( spritePosition, spriteRotation, spriteScale );
 
@@ -25647,42 +25657,49 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 			if ( oldFogType !== fogType ) {
 
-				gl.uniform1i( uniforms.fogType, fogType );
+				gl.uniform1i( program.uniforms.fogType, fogType );
 				oldFogType = fogType;
 
 			}
 
 			if ( material.map !== null ) {
 
-				gl.uniform2f( uniforms.uvOffset, material.map.offset.x, material.map.offset.y );
-				gl.uniform2f( uniforms.uvScale, material.map.repeat.x, material.map.repeat.y );
+				gl.uniform2f( program.uniforms.uvOffset, material.map.offset.x, material.map.offset.y );
+				gl.uniform2f( program.uniforms.uvScale, material.map.repeat.x, material.map.repeat.y );
 
 			} else {
 
-				gl.uniform2f( uniforms.uvOffset, 0, 0 );
-				gl.uniform2f( uniforms.uvScale, 1, 1 );
+				gl.uniform2f( program.uniforms.uvOffset, 0, 0 );
+				gl.uniform2f( program.uniforms.uvScale, 1, 1 );
 
 			}
 
-			gl.uniform1f( uniforms.opacity, material.opacity );
-			gl.uniform3f( uniforms.color, material.color.r, material.color.g, material.color.b );
+			gl.uniform1f( program.uniforms.opacity, material.opacity );
+			gl.uniform3f( program.uniforms.color, material.color.r, material.color.g, material.color.b );
 
-			gl.uniform1f( uniforms.rotation, material.rotation );
-			gl.uniform2fv( uniforms.scale, scale );
+			gl.uniform1f( program.uniforms.rotation, material.rotation );
+			gl.uniform2fv( program.uniforms.scale, scale );
+
+      // TODO:  Is there another place we should put the zOffset rather than on the material like this ?
+			gl.uniform1f( program.uniforms.zOffset, sprite.material.zOffset || 0.0 );
 
 			renderer.setBlending( material.blending, material.blendEquation, material.blendSrc, material.blendDst );
 			renderer.setDepthTest( material.depthTest );
 			renderer.setDepthWrite( material.depthWrite );
 
-			if ( material.map && material.map.image && material.map.image.width ) {
+      if ( !fragmentUniforms ) {
 
-				renderer.setTexture( material.map, 0 );
+			  if ( material.map && material.map.image && material.map.image.width ) {
 
-			} else {
+				  renderer.setTexture( material.map, 0 );
 
-				renderer.setTexture( texture, 0 );
+			  } else {
 
-			}
+				  renderer.setTexture( texture, 0 );
+
+			  }
+
+      }
 
 			gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0 );
 
@@ -25696,7 +25713,28 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 	};
 
-	function createProgram () {
+
+  function resolveShader( fragmentSource, fragmentUniforms ) {
+
+    for ( var pi = 0, plen = programs.length; pi < plen; pi++ ) {
+      var p = programs[ pi ];
+
+      if ( p.fragmentSource === fragmentSource ) {
+        return p;
+      }
+    }
+
+    var program = new WebGLSpriteProgram( fragmentSource, fragmentUniforms );
+    programs.push( program );
+    return program;
+  }
+
+
+  /**
+   * Similar to WebGLProgram except that it is designed to work with sprites.  This program allows you to provide your own
+   * fragment shader.
+   */
+	function WebGLSpriteProgram ( fragmentSource, fragmentUniforms ) {
 
 		var program = gl.createProgram();
 
@@ -25713,15 +25751,16 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 			'uniform vec2 scale;',
 			'uniform vec2 uvOffset;',
 			'uniform vec2 uvScale;',
+			'uniform float zOffset;',
 
 			'attribute vec2 position;',
 			'attribute vec2 uv;',
 
-			'varying vec2 vUV;',
+			'varying vec2 vUv;',
 
 			'void main() {',
 
-				'vUV = uvOffset + uv * uvScale;',
+				'vUv = uvOffset + uv * uvScale;',
 
 				'vec2 alignedPosition = position * scale;',
 
@@ -25733,6 +25772,7 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 				'finalPosition = modelViewMatrix * vec4( 0.0, 0.0, 0.0, 1.0 );',
 				'finalPosition.xy += rotatedPosition;',
+        'finalPosition.z += zOffset;',
 				'finalPosition = projectionMatrix * finalPosition;',
 
 				'gl_Position = finalPosition;',
@@ -25741,8 +25781,11 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 		].join( '\n' ) );
 
-		gl.shaderSource( fragmentShader, [
+    if ( fragmentSource ) {
+      this.fragmentSource = fragmentSource;
+    }
 
+		gl.shaderSource( fragmentShader, fragmentSource ? fragmentSource : [
 			'precision ' + renderer.getPrecision() + ' float;',
 
 			'uniform vec3 color;',
@@ -25756,11 +25799,11 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 			'uniform float fogFar;',
 			'uniform float alphaTest;',
 
-			'varying vec2 vUV;',
+			'varying vec2 vUv;',
 
 			'void main() {',
 
-				'vec4 texture = texture2D( map, vUV );',
+				'vec4 texture = texture2D( map, vUv );',
 
 				'if ( texture.a < alphaTest ) discard;',
 
@@ -25799,9 +25842,82 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 		gl.linkProgram( program );
 
-		return program;
+    if ( fragmentSource ) {
+      // assume the built-in shaders linked without error
 
-	};
+      if ( gl.getProgramParameter( program, gl.LINK_STATUS ) === false ) {
+
+			  console.error( 'THREE.WebGLSpriteProgram: Could not initialise shader.' );
+			  console.error( 'gl.VALIDATE_STATUS', gl.getProgramParameter( program, gl.VALIDATE_STATUS ) );
+			  console.error( 'gl.getError()', gl.getError() );
+
+		  }
+
+		  if ( gl.getProgramInfoLog( program ) !== '' ) {
+
+			  console.warn( 'THREE.WebGLSpriteProgram: gl.getProgramInfoLog()', gl.getProgramInfoLog( program ) );
+
+		  }
+    }
+
+		this.program = program;
+
+		this.attributes = {
+			position:	        gl.getAttribLocation ( program, 'position' ),
+			uv:					      gl.getAttribLocation ( program, 'uv' )
+		};
+
+		this.uniforms = {
+			uvOffset:			    gl.getUniformLocation( program, 'uvOffset' ),
+			uvScale:			    gl.getUniformLocation( program, 'uvScale' ),
+
+			rotation:			    gl.getUniformLocation( program, 'rotation' ),
+			scale:				    gl.getUniformLocation( program, 'scale' ),
+
+			zOffset:			    gl.getUniformLocation( program, 'zOffset' ),
+
+			color:				    gl.getUniformLocation( program, 'color' ),
+			map:				      gl.getUniformLocation( program, 'map' ),
+			opacity:			    gl.getUniformLocation( program, 'opacity' ),
+
+			modelViewMatrix: 	gl.getUniformLocation( program, 'modelViewMatrix' ),
+			projectionMatrix:	gl.getUniformLocation( program, 'projectionMatrix' ),
+
+			fogType:			    gl.getUniformLocation( program, 'fogType' ),
+			fogDensity:		    gl.getUniformLocation( program, 'fogDensity' ),
+			fogNear:			    gl.getUniformLocation( program, 'fogNear' ),
+			fogFar:				    gl.getUniformLocation( program, 'fogFar' ),
+			fogColor:			    gl.getUniformLocation( program, 'fogColor' ),
+
+			alphaTest:		    gl.getUniformLocation( program, 'alphaTest' )
+		};
+
+    if ( fragmentUniforms ) {
+
+      for ( var u in fragmentUniforms ) {
+
+        this.uniforms[ u ] = gl.getUniformLocation( program, u );
+
+      }
+
+    }
+
+    this.loadUniformsGeneric = function( fragmentUniforms ) {
+
+      var uniformsList = [];
+
+      for ( var u in fragmentUniforms ) {
+
+		    uniformsList.push( [ fragmentUniforms[ u ], this.uniforms[ u ] ] );
+
+      }
+
+      THREE.loadUniformsGeneric( uniformsList );
+
+    };
+
+		return this;
+	}
 
 	function painterSortStable ( a, b ) {
 
@@ -25815,7 +25931,7 @@ THREE.SpritePlugin = function ( renderer, sprites ) {
 
 		}
 
-	};
+	}
 
 };
 
