f028831292d857995ece7d44a7917d3b89201295
Dimensia <ted@dimensiology.com>
2019-06-10 15:54:17 -0500

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+
+export = THREE;
+
+type THREE = {
+  PlaneBufferGeometry: any;
+};
+
+module THREE {
+
+  export type Color = any;
+
+  export type Camera = any;
+
+  export type ImageTexture = any;
+
+  export type MaterialOptions = any;
+
+  export type Material = any;
+
+  export type Matrix4 = any;
+
+  export type MeshBasicMaterial = any;
+
+  export type RingGeometry = any;
+
+  export type Scene = any;
+
+  export type Shape = any;
+
+  export type Sprite = any;
+
+  export type Texture = any;
+
+  export type Vector2 = any;
+  export type Vector3 = any;
+
+  export type Object3D = {
+    position: Vector3D;
+    scale: Vector3D;
+    children: Object3D[];
+    userData: any;
+    visible?: boolean;
+    material: Material;
+
+    add(obj: Object3D): void;
+  }
+
+  export type Uniforms = any;
+}
+
+export const THREE;
+
